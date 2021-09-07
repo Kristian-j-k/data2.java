537d6a7eb95a922be902e4e3c2d151aabc6f3296
Kristian-j-k <89967661+Kristian-j-k@users.noreply.github.com>
2021-09-07 10:14:13 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // printer datoen amerikansk version, og europæisk version
+        String day = "Tuesday";
+        String date = "7th";
+        String month = "September";
+        int year = 2021;
+
+        System.out.println(day+", "+month+", "+date+", "+day+", "+year);
+        System.out.println(day+", "+month+", "+day+", "+date+", "+year);
+    }
+}
